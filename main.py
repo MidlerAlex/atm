7c500f7076595a19f350c83495673ff4acc4eec2
Aleksander Midler <rainmidler@mail.ru>
2026-08-19 22:10:22 +0700

    add foolproof

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 amount_money = 1000.00
 
-user_request = float(input("Введите сумму для снятия: "))
 
+def request_money(amount_money) -> float:
+    while True:
+        user_request = input("Введите сумму для снятия: ")
+        try:
+            user_request = float(user_request)
+
+        except ValueError:
+            print("Требуется ввести число")
+            continue
+
+        if user_request < 0:
+            print("Сумма должна быть положительной")
+
+        elif user_request > amount_money:
+            print("Недостаточно средств")
+        else:
+            return user_request
+
+
+user_request = request_money(amount_money)
 amount_money -= user_request
 
 print(f"Вы сняли: {user_request: .2f}")
